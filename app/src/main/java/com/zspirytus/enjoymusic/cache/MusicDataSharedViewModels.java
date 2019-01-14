ec5562c3a8ec1564b4dc1a9daa3b662283414b96
zkw012300 <zhangwel261717@gmail.com>
2019-01-14 23:54:19 +0800

    add ViewModel & LiveData to share music data to all UI.

---
+++ app/src/main/java/com/zspirytus/enjoymusic/cache/MusicDataSharedViewModels.java
@@ -0,0 +1,60 @@
+package com.zspirytus.enjoymusic.cache;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.zspirytus.enjoymusic.entity.Album;
+import com.zspirytus.enjoymusic.entity.Artist;
+import com.zspirytus.enjoymusic.entity.FolderSortedMusic;
+import com.zspirytus.enjoymusic.entity.Music;
+
+import java.util.List;
+
+public class MusicDataSharedViewModels extends ViewModel {
+
+    private MutableLiveData<List<Music>> mMusicListLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<Album>> mAlbumListLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<Artist>> mArtistListLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<FolderSortedMusic>> mFolderListLiveData = new MutableLiveData<>();
+    private MutableLiveData<Music> mCurrentMusicLiveData = new MutableLiveData<>();
+
+    public MutableLiveData<List<Music>> getMusicList() {
+        return mMusicListLiveData;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        mMusicListLiveData.setValue(musicList);
+    }
+
+    public MutableLiveData<List<Album>> getAlbumListLive() {
+        return mAlbumListLiveData;
+    }
+
+    public void setAlbumList(List<Album> albumList) {
+        mAlbumListLiveData.setValue(albumList);
+    }
+
+    public MutableLiveData<List<Artist>> getArtistList() {
+        return mArtistListLiveData;
+    }
+
+    public void setArtistList(List<Artist> artistList) {
+        mArtistListLiveData.setValue(artistList);
+    }
+
+    public MutableLiveData<List<FolderSortedMusic>> getFolderList() {
+        return mFolderListLiveData;
+    }
+
+    public void setFolderList(List<FolderSortedMusic> folderList) {
+        mFolderListLiveData.setValue(folderList);
+    }
+
+    public MutableLiveData<Music> getCurrentMusic() {
+        return mCurrentMusicLiveData;
+    }
+
+    public void setCurrentMusic(Music currentMusic) {
+        mCurrentMusicLiveData.setValue(currentMusic);
+    }
+}
