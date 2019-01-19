5a3fd5487c2aef33becc1ab5bcbe295476b581a0
zkw012300 <zhangwel261717@gmail.com>
2019-01-19 23:33:03 +0800

    add toolbar.

--- app/src/main/java/com/zspirytus/enjoymusic/base/CommonHeaderBaseFragment.java
+++ app/src/main/java/com/zspirytus/enjoymusic/base/CommonHeaderBaseFragment.java
@@ -2,8 +2,7 @@ package com.zspirytus.enjoymusic.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatImageView;
-import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.zspirytus.enjoymusic.R;
@@ -13,20 +12,15 @@ import org.simple.eventbus.EventBus;
 
 public abstract class CommonHeaderBaseFragment extends BaseFragment {
 
-    protected AppCompatImageView mNavIcon;
-    protected AppCompatTextView mTitle;
-    protected View mStatusBar;
-    protected View mToolbar;
+    protected Toolbar mToolbar;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mNavIcon = view.findViewById(R.id.common_header_nav_btn);
-        mTitle = view.findViewById(R.id.common_header_title);
-        mStatusBar = view.findViewById(R.id.status_bar);
         mToolbar = view.findViewById(R.id.tool_bar);
-        mNavIcon.setOnClickListener((v) ->
-                EventBus.getDefault().post(true, Constant.EventBusTag.OPEN_DRAWER)
-        );
+        mToolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setNavigationOnClickListener((v -> EventBus.getDefault().post(true, Constant.EventBusTag.OPEN_DRAWER)));
     }
+
 }
